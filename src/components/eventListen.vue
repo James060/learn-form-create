2b5b04b1348aa63be20e839092a40e9dd0e04681
James <384427051@qq.com>
2021-02-26 00:26:31 +0800

    事件监听的demo

---
+++ src/components/eventListen.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <form-create
+      v-model="fApi"
+      :option="options"
+      :rule="rule"
+    ></form-create>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'eventListen',
+  data() {
+    return {
+      fApi: {},
+      options: {
+        onSubmit: (formData) => {
+          alert(JSON.stringify(formData));
+        },
+      },
+      rule: [
+        {
+          type: 'input',
+          field: 'event',
+          title: 'change事件',
+          on: {
+            'on-change': () => {
+              alert(this.fApi.getValue('event'));
+            },
+          },
+        },
+      ],
+    };
+  },
+}
+</script>
+
+<style scoped>
+.container{
+  width: 700px;
+  margin: 0 auto;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 
-Vue.config.productionTip = false
+import formCreate from '@form-create/iview4';
+
+Vue.use(iView);
+Vue.use(formCreate);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
